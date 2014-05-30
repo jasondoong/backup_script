7af8e5b7b8cab5332d1b651dd56bfefe9c09e231
Jason Dong <jasondoong@gmail.com>
2014-05-30 19:12:37 +0800

    Add command line option to assign source and target folder

--- main.rb
+++ main.rb
@@ -1,10 +1,88 @@
+
 require_relative 'backup'
+require 'optparse'
 
-sourcePath = './ori/'
-targetPath = './target/'
+class Main
+    DefaultSourcePath = './ori/'
+    DefaultTargetPath = './target/'
+    
+    @pair = nil
+    @backupAciton = nil
+    @sourceFromUser = nil
+    @targetFromUser = nil
+    
+    def execute
+        parseSourceTargetFromUser
+        setSourceTarget
+        executeBackup
+        printExecutionResult
+    end
+    
+    private
+    def setSourceTarget
+        if(isUserAssignSourceAndTarget)
+            setSourceTargetFromUser
+        else
+            setDefaultSourceTarget
+        end
+    end
+    
+    def executeBackup
+        initialBackupAction
+        executeBackupAction
+    end
+    
+    def printExecutionResult
+        @backupAction.printResult
+    end
+    
+    def isUserAssignSourceAndTarget
+        if(@sourceFromUser!=nil && @targetFromUser!=nil)
+            true
+        else
+            false
+        end
+    end
+    
+    def setSourceTargetFromUser
+        @pair = getSourceTargetPair @sourceFromUser, @targetFromUser
+    end
+    
+    def setDefaultSourceTarget
+        @pair = getSourceTargetPair DefaultSourcePath, DefaultTargetPath
+    end
+    
+    def initialBackupAction
+        @backupAction = BackupAction.new(@pair)
+    end
+    
+    def executeBackupAction
+        @backupAction.backup
+    end
+    
+    def getSourceTargetPair(source,target)
+        SourceTargetPair.new source, target
+    end
+    
+    def parseSourceTargetFromUser
+        OptionParser.new do |opts|
+            opts.banner = 'Usage: main.rb [options]'
+            opts.on('-h', '--help', 'Help') do
+                puts opts
+                exit
+            end
+            
+            opts.on('-f', '--from source', 'From which folder') do |source|
+                @sourceFromUser = source
+            end
+            
+            opts.on('-t', '--to target', 'To which folder') do |target|
+                @targetFromUser = target
+            end
+            opts.parse!
+        end
+    end
+    
+end
 
-pair = SourceTargetPair.new(sourcePath, targetPath)
-
-backupAction = BackupAction.new(pair)
-backupAction.backup
-backupAction.printResult
+m = Main.new.execute
